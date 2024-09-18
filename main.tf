827a726f179b536185f674e5852618ed92c32d85
Shivamtiwari17297 <69217858+Shivamtiwari17297@users.noreply.github.com>
2024-09-18 17:00:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,42 +5,42 @@ provider "aws" {
 # Variables for existing resources
 variable "vpc_id" {
   description = "ID of the existing VPC"
-  default     = "vpc-02d9ba865bd886310"  # Replace with your existing VPC ID
+  default     = "vpc-07237fbcbcdc537d1"  # Replace with your existing VPC ID
 }
 
 variable "subnet_a_id" {
   description = "ID of the existing subnet A"
-  default     = "subnet-08624f3ea79a650c6"  # Replace with your existing subnet A ID
+  default     = "subnet-0d55ec792478c3859"  # Replace with your existing subnet A ID
 }
 
 variable "subnet_b_id" {
   description = "ID of the existing subnet B"
-  default     = "subnet-0c1d1e3c7f5d4e2df"  # Replace with your existing subnet B ID
+  default     = "subnet-0a575292cd2727788"  # Replace with your existing subnet B ID
 }
 
 variable "security_group_instance_id" {
   description = "ID of the existing Instance Security Group"
-  default     = "sg-05003d578620a6517"  # Replace with your existing Instance Security Group ID
+  default     = "sg-02d92909948cffb2b"  # Replace with your existing Instance Security Group ID
 }
 
 variable "igw_id" {
   description = "ID of the existing Internet Gateway"
-  default     = "igw-0d341c342e1331e7f"  # Replace with your existing Internet Gateway ID
+  default     = "igw-081c4bc2419fe9ce7"  # Replace with your existing Internet Gateway ID
 }
 
 variable "route_table_id" {
   description = "ID of the existing Route Table"
-  default     = "rtb-00caf7699da658465"  # Replace with your existing Route Table ID
+  default     = "rtb-08a6a402509fad8e5"  # Replace with your existing Route Table ID
 }
 
-variable "key_name" {
-  description = "Name of the existing SSH key pair"
-  default     = "an"  # Replace with your key pair name
-}
+#variable "key_name" {
+ # description = "Name of the existing SSH key pair"
+  #default     = "an"  # Replace with your key pair name
+#}
 
-# Launch t4g.medium instance
+# Launch  instance
 resource "aws_instance" "web" {
-  ami           = "ami-0ebfd941bbafe70c6"  # Replace with your desired ARM-compatible AMI ID
+  ami           = "ami-037774efca2da0726"  # Replace with your desired ARM-compatible AMI ID
   instance_type = "t3.medium"
   subnet_id     = var.subnet_a_id
   security_groups = [var.security_group_instance_id]
@@ -50,7 +50,7 @@ resource "aws_instance" "web" {
   # we assume you will add the EBS volume via the AWS Console.
 
   tags = {
-    Name = "web-instance"
+    Name = "test-server"
   }
 }
 
