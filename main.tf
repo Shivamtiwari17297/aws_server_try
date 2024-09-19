cb3e83cdcfb5053fb056c52c8aca0bdca36bdfff
Shivamtiwari17297 <69217858+Shivamtiwari17297@users.noreply.github.com>
2024-09-19 11:05:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,9 +38,9 @@ variable "key_name" {
 }
 
 # Launch  instance
-resource "aws_instance" "test-server" {
+resource "aws_instance" "server" {
   ami           = "ami-037774efca2da0726"  # Replace with your desired ARM-compatible AMI ID
-  instance_type = "t3.medium"
+  instance_type = "t2.micro"
   subnet_id     = var.subnet_a_id
   security_groups = [var.security_group_instance_id]
   key_name       = var.key_name
@@ -49,7 +49,7 @@ resource "aws_instance" "test-server" {
   # we assume you will add the EBS volume via the AWS Console.
 
   tags = {
-    Name = "test-server"
+    Name = "server"
   }
 }
 
