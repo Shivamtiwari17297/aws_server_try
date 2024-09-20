edeee7b0b72abb56ee7c941f6728c7f1394d89aa
Shivamtiwari17297 <69217858+Shivamtiwari17297@users.noreply.github.com>
2024-09-20 10:26:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,53 @@
+# main.tf
+
+# Create an AWS EC2 instance
+resource "aws_instance" "Prod-server" {                                                  # Prod-Singapore-Prod-API
+  ami                          = "ami-08718895af4dfa033"  # Replace with your desired AMI ID
+  instance_type                =  var.instance_type        # Replace with your desired instance type
+  subnet_id                    ="subnet-0d4f7782aa963e139" # Avialibility Zone ap-southeast-1
+vpc_security_group_ids = [
+    "sg-0bbc16fd865554ca4",
+    # "sg-06ef6aef694c0f34e"    ## API and game dono me 2nd wala alag hai
+
+
+    ]
+  associate_public_ip_address  = true
+key_name = "bigboss-pee"
+  count         = var.instance_count
+ # count = 0
+    user_data = <<-EOF
+    #cloud-config
+    hostname: instance-${count.index}
+    fqdn: instance-${count.index + 1}.example.com
+    manage_etc_hosts: true
+
+    runcmd:
+      - hostnamectl set-hostname Prod-server-${count.index + 1}    ## Name change API or game. 
+    EOF
+
+
+
+tags = {
+
+ Name = "Prod-server-${count.index + 1}"   ## Name change API or game.
+    Environment = "Prod"
+  }
+
+
+root_block_device {
+    volume_size = var.root_volume_size  # Replace with your desired root volume size in GB
+    volume_type = "gp2"  # Replace with your desired root volume type
+  }
+
+
+  # iam_instance_profile = "aws-ec2-prod-role"
+# Enable termination protection and stop protection
+  disable_api_termination = true
+
+
+
+}
+
+output "Prod-server" {                               ## Name change API or game.
+  value = aws_instance.Prod-server[*].public_ip      ## Name change API or game.
+}
