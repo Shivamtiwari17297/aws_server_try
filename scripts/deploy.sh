45057f695dd28337b71a3e5e5cf651af58306dc4
Shivamtiwari17297 <69217858+Shivamtiwari17297@users.noreply.github.com>
2024-09-23 12:50:53 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,28 +2,37 @@
 
 # Update system
 echo "Updating system..."
-sudo dnf update -y
+sudo yum update -y
 
 # Install Java
 echo "Installing Java..."
-sudo dnf install -y java-1.8.0-openjdk
+sudo yum install -y java-1.8.0-openjdk
 
 # Install Redis
 echo "Installing Redis..."
-sudo dnf install -y redis
+sudo yum install -y redis
 sudo systemctl enable redis
 sudo systemctl start redis
 
 # Install Filebeat
 echo "Installing Filebeat..."
-sudo dnf install -y filebeat
+sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+cat <<EOF | sudo tee /etc/yum.repos.d/elastic-7.x.repo
+[elastic-7.x]
+name=Elastic repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+enabled=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+EOF
+sudo yum install -y filebeat
 sudo systemctl enable filebeat
 sudo systemctl start filebeat
 
 # Install MySQL 8.0
 echo "Installing MySQL 8.0..."
-sudo dnf install -y https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
-sudo dnf install -y mysql-community-server
+sudo yum localinstall -y https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+sudo yum install -y mysql-community-server
 sudo systemctl enable mysqld
 sudo systemctl start mysqld
 
