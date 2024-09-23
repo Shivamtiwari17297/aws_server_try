b1cc00aa8309ece87414b4b721ce644c247e81b5
Shivamtiwari17297 <69217858+Shivamtiwari17297@users.noreply.github.com>
2024-09-23 15:10:16 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,15 @@
 
 # Update system
 echo "Updating system..."
-sudo yum update -y
+sudo dnf update -y
 
 # Install Java
 echo "Installing Java..."
-sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install -y java-11-openjdk
 
 # Install Redis
 echo "Installing Redis..."
-sudo amazon-linux-extras install redis4.0 -y
+sudo dnf install -y redis
 sudo systemctl enable redis
 sudo systemctl start redis
 
@@ -24,13 +24,19 @@ gpgcheck=1
 enabled=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 EOL
-sudo yum install -y filebeat
+sudo dnf install -y filebeat
 sudo systemctl enable filebeat
 sudo systemctl start filebeat
 
 # Install MySQL 8.0
 echo "Installing MySQL 8.0..."
-sudo yum install -y https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
-sudo yum install -y mysql-community-server
+sudo dnf install -y https://dev.mysql.com/get/mysql80-community-release-el8-3.noarch.rpm
+sudo dnf install -y mysql-community-server
 sudo systemctl enable mysqld
-sudo systemctl start mys
+sudo systemctl start mysqld
+
+# Check installed services status
+echo "Checking installed services status..."
+sudo systemctl status redis
+sudo systemctl status filebeat
+sudo systemctl status mysqld
